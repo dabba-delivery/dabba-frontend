508baf3c53f6110489fa0b957feb2bef32851b86
rudensergey <rudensergey@gmail.com>
2020-05-16 19:48:51 +0300

    add structure for the sort block

--- src/index.js
+++ src/index.js
@@ -2,16 +2,15 @@ import React from "react";
 import ReactDOM from "react-dom";
 import * as serviceWorker from "./serviceWorker";
 
-import Position from "./position";
+import Menu from "./menu";
 import "./index.css";
-import "./menu.css";
 
 const positions = [
     {
         name: "Пицца Маргарита",
         pictureUrl: "string",
-        weigh: 1000,
-        price: 1000,
+        weigh: 200,
+        price: 3400,
         description:
             "Картофель из печи, соленые огурчики, цыпленок, соус ранч, томаты, красный лук, чеснок, моцарелла, томатный соус",
         comments: "string",
@@ -20,7 +19,7 @@ const positions = [
         name: "Пицца Маргарита",
         pictureUrl: "string",
         weigh: 1000,
-        price: 1000,
+        price: 800,
         description:
             "Картофель из печи, соленые огурчики, цыпленок, соус ранч, томаты, красный лук, чеснок, моцарелла, томатный соус",
         comments: "string",
@@ -29,7 +28,7 @@ const positions = [
         name: "Пицца Маргарита",
         pictureUrl: "string",
         weigh: 1000,
-        price: 1000,
+        price: 50,
         description:
             "Картофель из печи, соленые огурчики, цыпленок, соус ранч, томаты, красный лук, чеснок, моцарелла, томатный соус",
         comments: "string",
@@ -37,41 +36,37 @@ const positions = [
     {
         name: "Пицца Маргарита",
         pictureUrl: "string",
-        weigh: 1000,
-        price: 1000,
+        weigh: 1400,
+        price: 1500,
         description:
             "Картофель из печи, соленые огурчики, цыпленок, соус ранч, томаты, красный лук, чеснок, моцарелла, томатный соус",
         comments: "string",
     },
 ];
 
-function UnpackPositions(props) {
-    const positionsArr = props.arr;
-    let listPositions = positionsArr.map((position) => {
-        return (
-            <Position
-                id={position}
-                name={position.name}
-                ingridients={position.description}
-                weight={position.weigh}
-                cost={position.price}
-            />
-        );
-    });
-
-    return listPositions;
-}
-
-class Menu extends React.Component {
+class SortBlock extends React.Component {
     render() {
         return (
-            <div className="menu">
-                <UnpackPositions arr={positions} />
+            <div className="sort">
+                <h5 className="sort__text">Показать</h5>
+                <button className="sort__new">Новинки</button>
+                <button className="sort__popular">Популярное</button>
             </div>
         );
     }
 }
 
-ReactDOM.render(<Menu />, document.getElementById("root"));
+class MainPart extends React.Component {
+    render() {
+        return (
+            <div>
+                <SortBlock />
+                <Menu dishes={positions} />
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<MainPart />, document.getElementById("root"));
 
 serviceWorker.unregister();
---
+++ src/menu.js
@@ -0,0 +1,45 @@
+import React from "react";
+import Position from "./position";
+import "./menu.css";
+
+// компонент Menu принимает в себя массив объектов блюд следующего формата
+// и автоматически распаковывает его
+
+// let arr = [
+//     {
+//         name: "Пицца Маргарита",
+//         pictureUrl: "string",
+//         weigh: 1000,
+//         price: 1000,
+//         description:
+//             "Картофель из печи, соленые огурчики, цыпленок, соус ранч, томаты, красный лук, чеснок, моцарелла, томатный соус",
+//         comments: "string",
+//     },
+// ];
+
+export default class Menu extends React.Component {
+    render() {
+        return (
+            <div className="menu">
+                <UnpackPositions arr={this.props.dishes} />
+            </div>
+        );
+    }
+}
+
+function UnpackPositions(props) {
+    const positionsArr = props.arr;
+    let listPositions = positionsArr.map((position) => {
+        return (
+            <Position
+                id={position}
+                name={position.name}
+                ingridients={position.description}
+                weight={position.weigh}
+                cost={position.price}
+            />
+        );
+    });
+
+    return listPositions;
+}
