31da93c177f1d3da4372439ab05a1215c01377a8
rudensergey <rudensergey@gmail.com>
2020-05-16 14:57:11 +0300

    add basic style

--- src/index.js
+++ src/index.js
@@ -11,14 +11,16 @@ class Position extends React.Component {
     render() {
         return (
             <div className="position">
-                <img className="position__image" src={FoodImage} />
+                <div className="position__image"></div>
                 <h5 className="position__name">Пицца Маргарита</h5>
                 <p className="position__ingridients">
                     Картофель из печи, соленые огурчики, цыпленок, соус ранч,
                     томаты, красный лук, чеснок, моцарелла, томатный соус
                 </p>
-                <p className="postion__weight">500гр</p>
-                <p className="position__cost">650гр</p>
+                <div className="position__wc">
+                    <p className="postion__weight">500гр</p>
+                    <p className="position__cost">650гр</p>
+                </div>
                 <button className="position__button">Выбрать</button>
             </div>
         );
