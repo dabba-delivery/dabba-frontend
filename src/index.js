9f298867ff94c4aa42615f21b3d18d50af0ce78b
rudensergey <rudensergey@gmail.com>
2020-05-16 20:38:47 +0300

    finish to style

--- src/index.js
+++ src/index.js
@@ -4,8 +4,45 @@ import * as serviceWorker from "./serviceWorker";
 
 import Menu from "./menu";
 import "./index.css";
+import "./sort.css";
 
 const positions = [
+    {
+        name: "Пицца Маргарита",
+        pictureUrl: "string",
+        weigh: 200,
+        price: 3400,
+        description:
+            "Картофель из печи, соленые огурчики, цыпленок, соус ранч, томаты, красный лук, чеснок, моцарелла, томатный соус",
+        comments: "string",
+    },
+    {
+        name: "Пицца Маргарита",
+        pictureUrl: "string",
+        weigh: 1000,
+        price: 800,
+        description:
+            "Картофель из печи, соленые огурчики, цыпленок, соус ранч, томаты, красный лук, чеснок, моцарелла, томатный соус",
+        comments: "string",
+    },
+    {
+        name: "Пицца Маргарита",
+        pictureUrl: "string",
+        weigh: 1000,
+        price: 50,
+        description:
+            "Картофель из печи, соленые огурчики, цыпленок, соус ранч, томаты, красный лук, чеснок, моцарелла, томатный соус",
+        comments: "string",
+    },
+    {
+        name: "Пицца Маргарита",
+        pictureUrl: "string",
+        weigh: 1400,
+        price: 1500,
+        description:
+            "Картофель из печи, соленые огурчики, цыпленок, соус ранч, томаты, красный лук, чеснок, моцарелла, томатный соус",
+        comments: "string",
+    },
     {
         name: "Пицца Маргарита",
         pictureUrl: "string",
@@ -49,9 +86,29 @@ class SortBlock extends React.Component {
         return (
             <div className="sort">
                 <h5 className="sort__text">Показать</h5>
-                <button className="sort__new">Новинки</button>
-                <button className="sort__popular">Популярное</button>
+                <SortButton name="Новинки" func={() => alert("It works")} />
+                <SortButton name="Популярное" func={() => alert("It works")} />
             </div>
+        );
+    }
+}
+
+class SortButton extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleClick = this.handleClick.bind(this);
+    }
+
+    handleClick(e) {
+        e.preventDefault();
+        this.props.func();
+    }
+
+    render() {
+        return (
+            <button onClick={this.handleClick} className="sort__button">
+                {this.props.name}
+            </button>
         );
     }
 }
@@ -59,7 +116,12 @@ class SortBlock extends React.Component {
 class MainPart extends React.Component {
     render() {
         return (
-            <div>
+            <div
+                style={{
+                    backgroundColor: "#f7f7f7",
+                    padding: "10px 0px 0px 0px",
+                }}
+            >
                 <SortBlock />
                 <Menu dishes={positions} />
             </div>
