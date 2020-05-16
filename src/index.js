5ca24e5c19466530412f6c3905b4a33939457e87
rudensergey <rudensergey@gmail.com>
2020-05-16 13:54:47 +0300

    add classes and set styles with css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,29 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<h1>Hwllo!</h1>, document.getElementById("root"));
+import "./index.css";
+import "./position.css";
+import FoodImage from "./media/food.png";
+
+class Position extends React.Component {
+    render() {
+        return (
+            <div className="position">
+                <img className="position__image" src={FoodImage} />
+                <h5 className="position__name">Пицца Маргарита</h5>
+                <p className="position__ingridients">
+                    Картофель из печи, соленые огурчики, цыпленок, соус ранч,
+                    томаты, красный лук, чеснок, моцарелла, томатный соус
+                </p>
+                <p className="postion__weight">500гр</p>
+                <p className="position__cost">650гр</p>
+                <button className="position__button">Выбрать</button>
+            </div>
+        );
+    }
+}
+
+ReactDOM.render(<Position />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
