24955abc9c7e928a8dd4f8a448dbd7e591465267
rudensergey <rudensergey@gmail.com>
2020-05-23 02:33:45 +0300

    separate bin.js

--- src/sort.js
+++ src/sort.js
@@ -1,7 +1,7 @@
 import React from "react";
 import "./sort.css";
 
-import { Toggle } from "./components/library.js";
+import { Toggle, Button } from "./components/library.js";
 
 // внутри каждой кнопки сортировки есть func - сюда нужно
 // вставлять функцию сортировки от компонентов выше
@@ -12,20 +12,14 @@ export default class SortBlock extends React.Component {
         return (
             <div className="sort">
                 <h5 className="sort__text">Показать</h5>
-                <Toggle
-                    elementStyle={"orange"}
-                    
-                    handleFunction={() => console.log("sort works")}
-                >
-                    Наборы
-                </Toggle>
-                <Toggle
-                    elementStyle={"blue"}
-                    
-                    handleFunction={() => console.log("sort works")}
-                >
-                    Популярное
-                </Toggle>
+                <div className="sort__box">
+                    <Toggle
+                        elementStyle={"orange"}
+                        handleFunction={() => console.log("sort works")}
+                    >
+                        Наборы
+                    </Toggle>
+                </div>
             </div>
         );
     }
