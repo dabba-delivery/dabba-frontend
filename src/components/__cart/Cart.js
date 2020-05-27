5f09c58ab0f7b50696d3502d6c42a740a1fa38ab
rudensergey <rudensergey@gmail.com>
2020-05-27 20:44:50 +0300

    add delete button for position in the cart

--- src/components/__cart/Cart.js
+++ src/components/__cart/Cart.js
@@ -4,11 +4,18 @@ import "./--default.css";
 export class Cart extends React.Component {
     render() {
         return (
-            <div className="cart" key={this.props.key}>
-                <h5 className="cart__name">{this.props.position.name}</h5>
-                <p className="cart__amount">{this.props.amount}шт.</p>
-                <p className="cart__weight">{this.props.position.weigh}гр.</p>
-                <p className="cart__price">{this.props.position.price}р.</p>
+            <div className="cart" >
+                <div className="cart__block" key={this.props.key}>
+                    <h5 className="cart__name">{this.props.position.name}</h5>
+                    <p className="cart__amount">{this.props.amount}шт.</p>
+                    <p className="cart__weight">
+                        {this.props.position.weigh}гр.
+                    </p>
+                    <p className="cart__price">{this.props.position.price}р.</p>
+                </div>
+                <div className="cart__option">
+                    <p className="cart__delete" >Удалить</p>
+                </div>
             </div>
         );
     }
